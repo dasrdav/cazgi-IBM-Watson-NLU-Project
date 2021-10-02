47d5cb37c1fd78dd3557f84fa9f40bd217157ca4
Diogo Vieira <diogo1996@msn.com>
2021-10-02 18:25:17 +0000

    changes made from theia lab environment

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -77,3 +77,13 @@ let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');<pre><code>const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    version: '2021-08-01',
+    authenticator: new IamAuthenticator ({
+        apikey: api_key,
+    }),
+    serviceUrl: api_url,
+});
+return naturalLanguageUnderstanding
+</code></pre>
